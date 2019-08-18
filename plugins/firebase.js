0077ff31111353ed222ccd7484d1ca59556b847a
Inga Balcune <inga.balcune@gmail.com>
2019-08-18 19:35:32 +0200

    changes to firebase plugin

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import '@firebase/auth';
-import '@firebase/database';
+import '@firebase/database'
+import '@firebase/functions'
 
 let firebaseConfig = {
     apiKey: "AIzaSyAl5iJAnHm3G6HKxmkkJ-XHLrNF2KRPjKU",
